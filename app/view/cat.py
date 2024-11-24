e37c2f0f597957a758000a8e8c9eb6eb97430d95
grinvolod13 <grinvols@gmail.com>
2024-11-24 17:04:42 +0200

    add some cat endpoints

--- app/view/cat.py
+++ app/view/cat.py
@@ -5,31 +5,45 @@ from app.model import Mission, Target
 from app.model import Targets, UpdatedTargets, Salary, Cat # validation pydantic model
 from app.const import CAT_BREED_API
 from app.model import validation
+from app.controller import CatController
+
 
 router = APIRouter()
 
 
-@router.get('/')
+@router.get('/', response_model=list[Cat])
 def get_all(db: DB):
-    ...
+    return CatController(db).get_all()
 
-@router.get('/{id}')
+@router.get('/{id}', response_model=Cat)
 def get_single(id: int, db: DB):
-    ...
+    try:
+        return CatController(db).get(id)
+    except ValueError as e:
+        raise HTTPException(status_code=404, detail=e.args[0])
+        
 
 @router.delete('/{id}')
-def remove_mission(id: int, db: DB):
-    ...
+def fire_cat(id: int, db: DB):
+    try:
+        CatController(db).remove(id)
+    except ValueError as e:
+        raise HTTPException(status_code=404, detail=e.args[0])
 
 @router.put('/{id}/')
 def update_salary(id: int, salary: Salary, db: DB):
-    ...
+    try:
+        CatController(db).set_salary(id, salary)
+    except ValueError as e:
+        raise HTTPException(status_code=404, detail=e.args[0])
+    
 
 @router.post('/')
-async def add_cat(cat: Cat):
+async def add_cat(cat: Cat, db: DB):
     if not (await validation.validate_breed(cat)):
         raise HTTPException(
             status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
             detail=f"Unknown breed, check out {CAT_BREED_API} for correct breed names",
             )
+    return CatController(db).create(cat)
     
